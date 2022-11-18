dda9d25f643c71f450fa06a1cec04e5dba1967c6
HarnoorBajwa <harnoor63@gmail.com>
2022-11-17 21:37:20 -0400

    First Commit

---
+++ ConditionalsAndLoops/src/CalculatorProgram.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class CalculatorProgram {
+    public static void main(String[] args) {
+        Scanner kb = new Scanner(System.in);
+        // Take input from user till user does not press X or x
+    }
+
+}
