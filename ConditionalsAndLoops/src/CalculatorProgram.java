fac438051d2fbc53773e711159c8cd42df4ef278
HarnoorBajwa <harnoor63@gmail.com>
2022-11-18 22:32:52 -0400

    PRogram Completed

--- ConditionalsAndLoops/src/CalculatorProgram.java
+++ ConditionalsAndLoops/src/CalculatorProgram.java
@@ -1,19 +1,23 @@
 import java.util.Scanner;
 
+/**
+ * A Calculator program
+ */
 public class CalculatorProgram {
     public static void main(String[] args) {
         Scanner kb = new Scanner(System.in);
         // Take input from user till user does not press X or x
-        char op = kb.next().trim().charAt(0);
-        int ans;
+
+        int ans=0;
         while (true) {
             System.out.println("Enter an operator");
+            char op = kb.next().trim().charAt(0);
 
             if (op == '+' || op == '-' || op == '*' || op == '/' || op == '%') {
                 System.out.println("Enter two numbers");
 
-                int num1 = kb.nextInt();
-                int num2 = kb.nextInt();
+                int num1 = kb.nextInt(); int num2 = kb.nextInt();
+
                 if (op == '+') {
                     ans = num1 + num2;
                 }
@@ -27,6 +31,10 @@ public class CalculatorProgram {
                     if (num2 != 0) {
                         ans = num1 / num2;
                     }
+                    else{
+                        ans = 0;
+                    }
+
                 }
                 if (op == '%') {
                     ans = num1 % num2;
@@ -36,8 +44,10 @@ public class CalculatorProgram {
             }else{
                 System.out.println("Invalid Entry !");
             }
+            System.out.println(ans);
 
         }
+
 
 
 
