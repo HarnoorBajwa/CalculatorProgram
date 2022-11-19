026529726403fbd3606cfc411bba08afb96a27e3
HarnoorBajwa <harnoor63@gmail.com>
2022-11-18 20:14:35 -0400

    second commit

--- ConditionalsAndLoops/src/CalculatorProgram.java
+++ ConditionalsAndLoops/src/CalculatorProgram.java
@@ -3,7 +3,8 @@ import java.util.Scanner;
 public class CalculatorProgram {
     public static void main(String[] args) {
         Scanner kb = new Scanner(System.in);
-        // Take input from user till user does not press X or x
+        // Take input from user till user does not git apress X or x
     }
+
 
 }
