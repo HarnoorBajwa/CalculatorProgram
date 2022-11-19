62408e7a47f26a36e0c0da98d4ce9d56d58069f8
HarnoorBajwa <harnoor63@gmail.com>
2022-11-18 21:47:19 -0400

    added loops and conditional statements

--- ConditionalsAndLoops/src/CalculatorProgram.java
+++ ConditionalsAndLoops/src/CalculatorProgram.java
@@ -5,12 +5,41 @@ public class CalculatorProgram {
         Scanner kb = new Scanner(System.in);
         // Take input from user till user does not press X or x
         char op = kb.next().trim().charAt(0);
-        System.out.println("Enter an operator");
+        int ans;
+        while (true) {
+            System.out.println("Enter an operator");
 
-        if( op =='+' || op == '-' || op == '*' || op== '/' || op == '%'){
-            System.out.println("Enter two numbers");
+            if (op == '+' || op == '-' || op == '*' || op == '/' || op == '%') {
+                System.out.println("Enter two numbers");
+
+                int num1 = kb.nextInt();
+                int num2 = kb.nextInt();
+                if (op == '+') {
+                    ans = num1 + num2;
+                }
+                if (op == '-') {
+                    ans = num1 - num2;
+                }
+                if (op == '*') {
+                    ans = num1 * num2;
+                }
+                if (op == '/') {
+                    if (num2 != 0) {
+                        ans = num1 / num2;
+                    }
+                }
+                if (op == '%') {
+                    ans = num1 % num2;
+                }
+            }else if(op == 'x' || op == 'X'){
+                break;
+            }else{
+                System.out.println("Invalid Entry !");
+            }
+
         }
+
+
+
     }
-
-
 }
