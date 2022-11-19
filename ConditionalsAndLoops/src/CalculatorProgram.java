0ea2e14c3b01ec7889ea53f079d22368b1a86e12
HarnoorBajwa <harnoor63@gmail.com>
2022-11-18 21:36:19 -0400

    Initialized operator variable

--- ConditionalsAndLoops/src/CalculatorProgram.java
+++ ConditionalsAndLoops/src/CalculatorProgram.java
@@ -3,7 +3,13 @@ import java.util.Scanner;
 public class CalculatorProgram {
     public static void main(String[] args) {
         Scanner kb = new Scanner(System.in);
-        // Take input from user till user does not git apress X or x
+        // Take input from user till user does not press X or x
+        char op = kb.next().trim().charAt(0);
+        System.out.println("Enter an operator");
+
+        if( op =='+' || op == '-' || op == '*' || op== '/' || op == '%'){
+            System.out.println("Enter two numbers");
+        }
     }
 
 
